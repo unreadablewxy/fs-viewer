41ef0758e8fc1d0f2db2a7d3fba558289bbaf65d
Noramire <korewanoramire@gmail.com>
2020-04-05 19:04:01 +1000

    lint

--- src/api/files.ts
+++ src/api/files.ts
@@ -78,7 +78,7 @@ function findTagNamespaceFile(directory: string): string | null {
         if (existsSync(tryPath))
             return tryPath;
         else
-            writeFileSync(tryPath, '');
+            writeFileSync(tryPath, "");
     }
 
     return null;
