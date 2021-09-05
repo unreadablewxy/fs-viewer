d4093045697fd6a50b8db427828452a72f591777
wxy <wxy@unreadableco.de>
2021-09-04 23:33:14 -0700

    Snapshot from scm.treehouse.uc/fs-v source correlation id:885cf3e5-7a5a-41d4-9b32-b2e72936491d; job:b466736c-c2e5-4a7a-b51b-5c936b9a3ae6

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,37 @@
 "use strict";
 
-const {from} = require("webpack-config-builder");
+const {newConfigBuilder} = require("webpack-config-builder");
 const os = require("os");
 const path = require("path");
 
 const platform = os.platform();
 const pathBuild = path.resolve(__dirname, "build");
 
-var renderer = from("./src/application/index.ts")
-    .to("web", pathBuild, "[chunkhash].js", "dev.js")
-    .withUnacceptableLicense("GPL-3.0")
+const builder = newConfigBuilder()
+    .withoutLicense("GPL-3.0")
     .withDefine("BUILD_TYPE", "pub", "dev")
-    .withDefine("PLATFORM", os.platform())
+    .withDefine("PLATFORM", platform);
+
+var renderer = builder
     .withCss("index.css")
     .withReact()
     .withHtml("./src/application/index.html", "index.html")
     .withNoParse(/src(\\|\/)application(\\|\/)esimport.js$/);
 
-var api = from("./src/api/index.ts")
-    .to("electron-preload", pathBuild, "api.js")
-    .withUnacceptableLicense("GPL-3.0");
-
-var main = from("./src/main/index.ts")
-    .to("electron-main", pathBuild, "index.js")
-    .withUnacceptableLicense("GPL-3.0")
+var main = builder
     .withNativeModules()
-    .withDefine("BUILD_TYPE", "pub", "dev")
     .withExternals({
         bindings: `require("bindings")`,
     });
 
-    if (platform === "linux") {
-        main = main.withFiles([
-            { from: "node_modules/abstract-socket/build/Release/bindings.node" },
-        ]);
-    }
+if (platform === "linux") {
+    main = main.withFiles([
+        { from: "node_modules/abstract-socket/build/Release/bindings.node" },
+    ]);
+}
 
 module.exports = [
-    renderer.build(),
-    api.build(),
-    main.build(),
+    renderer.compile("web", "/src/application/index.ts", pathBuild, "[chunkhash].js", "dev.js"),
+    builder.compile("electron-preload", "/src/api/index.ts", pathBuild, "api.js"),
+    main.compile("electron-main", "/src/main/index.ts", pathBuild, "index.js"),
 ];
